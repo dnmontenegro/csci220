07384499774956185d143fe4e00e767ca9255768
David Montenegro <dnmontenegro@wm.edu>
2022-11-30 01:07:04 -0500

    Edit perceptron.c

--- ASSIGNMENT_11/main.c
+++ ASSIGNMENT_11/main.c
@@ -5,19 +5,15 @@
 int main(int argc, char *argv[])
 {
     char *fname = argv[1];
-    int number_of_examples = atoi(argv[2]);
+    //int number_of_examples = atoi(argv[2]);
 
     // Building
-    Data xcoords = new_Data(number_of_examples);
-    Data ycoords = new_Data(number_of_examples);
-    Data targets = new_Data(number_of_examples);
-    load_data(fname, xcoords, ycoords, targets);
+    Data data = new_Data(fname);
 
-    Model model = new_Model();
-    initialize_model(model);
+    Model model = new_Model(data);
 
     // Training
-    fit_model(model, xcoords, ycoords, targets, number_of_examples);
+    fit_model(model, data);
 
     // Testing
     // TODO
@@ -25,9 +21,7 @@ int main(int argc, char *argv[])
     // Scoring
     run_scoring_engine(model);
 
-    free(xcoords);
-    free(ycoords);
-    free(targets);
+    free(data);
     free(model);
 
     return 0;
--- ASSIGNMENT_11/perceptron.c
+++ ASSIGNMENT_11/perceptron.c
@@ -4,81 +4,103 @@
 #include "perceptron.h"
 
 struct data {
-    double *elements;
+    double **inputs;
+    int *targets;
+    struct shape1 {
+    	int number_of_examples;
+        int number_of_features;
+    } shape;
 };
 
 struct model {
     double *weights;
+    struct shape2 {
+	int DIMENSIONS;
+    } shape;
 };
 
-Data new_Data(int number_of_examples)
+Data new_Data(const char *fname)
 {
     Data data = (Data) malloc(sizeof(Data));
-    data->elements = (double*) malloc(number_of_examples * sizeof(double));
-    return data;
-}
-
-void load_data(char *fname, Data x, Data y, Data targets)
-{
     FILE *fp;
-
+    char ch;
+    int lines = 0;
+    if ((fp = fopen(fname, "r")) == NULL) {
+    	fprintf(stderr, "load_data: can't open %s\n", fname);
+        exit(1);
+    }
+    for (ch = getc(fp); ch != EOF; ch = getc(fp)) {
+	if (ch == '\n')
+  		lines++;
+    }
+    rewind(fp);
+    fclose(fp);
+    data->shape.number_of_examples = lines;
+    printf("%d\n", data->shape.number_of_examples);
+    data->shape.number_of_features = 2;
+    printf("%d\n", data->shape.number_of_features);
+    data->inputs = malloc(data->shape.number_of_features * sizeof(double*));
+    for(int i = 0; i < data->shape.number_of_features; i++)
+	data->inputs[i] = malloc(data->shape.number_of_examples * sizeof(double));
+    data->targets = (int*) malloc(data->shape.number_of_examples * sizeof(int));
     if ((fp = fopen(fname, "r")) == NULL)  {
         fprintf(stderr, "load_data: can't open %s\n", fname);
         exit(1);
     }
-
     int i = 0;
-    //while (fscanf(fp, "%lf %lf %lf", &x[i], &y[i], &targets[i]) != EOF)
-    while (fscanf(fp, "%lf %lf %lf", &(x->elements[i]), &(y->elements[i]), &(targets->elements[i])) != EOF)
-        i++;
-
+    printf("hello, there3\n");
+    while (fscanf(fp, "%lf %lf %lf", &(data->inputs[0][i]), &(data->inputs[1][i]), (int)&(data->targets[i])) != EOF)
+	i++;
     fclose(fp);
-
     fprintf(stdout, "load_data: loaded %d examples\n", i);
+    return data;
 }
 
-Model new_Model()
+Model new_Model(const Data data)
 {
     Model model = (Model) malloc(sizeof(Model));
-    model->weights = (double*) malloc(DIMENSIONS * sizeof(double));
+    model->shape.DIMENSIONS = data->shape.number_of_features + 1;
+    model->weights = (double*) malloc(model->shape.DIMENSIONS * sizeof(double));
+    for (int i = 0; i < model->shape.DIMENSIONS; i++)
+	model->weights[i] = (double) rand() / RAND_MAX;
+    printf("hello, there4\n");
     return model;
 }
 
-void initialize_model(Model model)
+static void sgd(Model model, Data data)
 {
-    for (int i = 0; i < DIMENSIONS; i++)
-        model->weights[i] = (double) rand() / RAND_MAX;
-}
-
-static void sgd(Model model, double xcoord, double ycoord, double target)
-{
-    model->weights[2] += target * xcoord;
-    model->weights[1] += target * ycoord;
+    model->weights[2] += data->targets[data->shape.number_of_features] * data->inputs[0][data->shape.number_of_features];
+    model->weights[1] += data->targets[data->shape.number_of_features] * data->inputs[1][data->shape.number_of_features];
     //weights[0] = weights[0];
-    model->weights[0] += target * 1;
+    model->weights[0] += data->targets[data->shape.number_of_features] * 1;
 }
 
-static int predict(Model model, double xcoord, double ycoord)
+static int predict(Model model, Data data)
 {
-    double hypothesis = model->weights[2] * xcoord + model->weights[1] * ycoord + model->weights[0];
+    double hypothesis = model->weights[2] * data->inputs[0][data->shape.number_of_features] + model->weights[1] * data->inputs[1][data->shape.number_of_features] + model->weights[0];
     return (hypothesis < 0) ? -1 : 1;
 }
 
-void fit_model(Model model, Data xcoords, Data ycoords, Data targets, int number_of_examples)
+void fit_model(Model model, Data data)
 {
     double hypothesis, target;
-
+   
     bool misclassified = true;
+    printf("hello, there5\n");
     while (misclassified) {
         misclassified = false;
-        for (int i = 0; i < number_of_examples; i++) {
-            hypothesis = predict(model, xcoords->elements[i], ycoords->elements[i]);
-            target = targets->elements[i];
+        for (int i = 0; i < data->shape.number_of_examples; i++) {
+	    data->shape.number_of_features = i;
+            hypothesis = predict(model, data);
+            target = data->targets[i];
+	    printf("%f\n", hypothesis);
+	    printf("%f\n", target);
             if ((hypothesis > 0 && target > 0) || (hypothesis < 0 && target < 0)) // TODO Handle 0
                 continue;
-            sgd(model, xcoords->elements[i], ycoords->elements[i], target);  // Update weights using misclassified point
+            sgd(model, data);  // Update weights using misclassified point
             misclassified = true;
         }
+        misclassified = false;
     }
 }
 
@@ -91,6 +113,7 @@ void run_scoring_engine(Model model)
 
     printf("Enter y: \n");
     scanf("%lf", &y);
+   // Data data = (Data) malloc(sizeof(Data));
 
-    printf("Prediction = %d\n", predict(model, x, y));
+    //printf("Prediction = %d\n", predict(model, data));
 }
